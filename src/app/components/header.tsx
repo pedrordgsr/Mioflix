35e260a78f4fa72b553f19a9e40195b36e8938d6
Pedro Rodrigues <pedrorodriguesssrr@gmail.com>
2023-10-09 21:16:42 -0300

    Ícones ajustados

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,17 +3,16 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-<div className="fixed top-0 left-0 z-40 w-28 h-screen flex flex-col justify-evenly pt-4 pb-4 px-5 text-lg backdrop-blur bg-neutral-900/30">
-<Link href="/"><i className="fi fi-rr-user"></i></Link>
-        <h1 className='me-5'>Contato</h1>
+<div className="fixed top-0 left-0 z-40 w-16 h-screen flex flex-col justify-evenly pt-4 pb-4 px-5 text-lg backdrop-blur bg-neutral-900/30">
+<Link href="/"><img src="/images/home.png"></img></Link>
+<Link href="/"><img src="/images/menu.png"></img></Link>        
    <Link href="/login">     <div className='flex'>
         <Image className='rounded-full me-4'
       src="/avatar.jpg"
-      width={30}
-      height={30}
+      width={32}
+      height={32}
       alt="Picture of the author"
     />
-            <h1>Perfil</h1>
         </div></Link>
         
 </div>
